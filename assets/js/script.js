e4dc214f1dd92c4e75089f1633d1def8dd9a4205
Joel C <joel7921111@yahoo.com>
2022-02-18 15:03:21 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+js will:
+    -start quiz timer at 75 s -
+    deduct 10 s
+for every question answered incorrectly
+    -
+    dynamically create quiz questions -
+    display whether answers are correct or incorrect upon selection(click) -
+    store high score data and display them upon completion of quiz
