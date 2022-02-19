0c37a662c975f75f2abce14237c1175ccf95d4d5
Joel C <joel7921111@yahoo.com>
2022-02-19 14:49:42 -0700

    index.html and css initial setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-js will:
-    -start quiz timer at 75 s -
-    deduct 10 s
-for every question answered incorrectly
-    -
-    dynamically create quiz questions -
-    display whether answers are correct or incorrect upon selection(click) -
-    store high score data and display them upon completion of quiz
+// js will:
+//     -start quiz timer at 75 s 
+//          -crate  a var for timer
+// -set it to 75
+// -display it in Header
+
+//     -deduct 10s for every question answered incorrectly
+//     -dynamically create quiz questions
+//     -display whether answers are correct or incorrect upon selection(click) 
+//     -store high score data and display them upon completion of quiz
