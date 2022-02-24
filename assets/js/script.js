2c08119cf44bab58a24ffd42a69ea7ba529f5d4b
Joel C <joel7921111@yahoo.com>
2022-02-24 14:33:25 -0700

    updated logic w suggestions from ta, refactored question content

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@
 //     -dynamically create quiz questions
 //     -display whether answers are correct or incorrect upon selection(click) 
 //     -store high score data and display them upon completion of quiz
-
+var QuestIndex = 0;
 var startButtonEl = document.querySelector("#start-button");
 var timerEl = document.querySelector("#time");
 var timeLeft = 75;
@@ -23,7 +23,7 @@ timerEl.innerHTML = timeLeft;
 
 
 function countDown() {
-    timeLeft = 75;
+    // timeLeft = 75;
     var countInterval = setInterval(function() {
         if (timeLeft === 0) {
             clearInterval(countInterval);
@@ -39,100 +39,104 @@ function quizStart() {
     startButtonEl.remove();
     questionAnswerEl.style.display = "block";
     countDown();
-    quiz();
+    nextQuestion(QuestIndex);
 };
 
 
-var quizQuestions = {
-    one: "What does the acronym HTML stand for?",
-    two: "What does the acronym CSS stand for?",
-    three: "How does Java relate to JavaScript?",
-    four: "In CSS, what defines class?",
-    five: "In Javascript, what does JSON.parse do?",
-    six: "In HTML, what does the <p> tag represent?",
-    seven: "Which of these If statements is in the correct order?",
-    eight: "Which is NOT a date type?",
-    nine: "Which of these stops a loop?",
-    ten: "What is the terminology for a loop that doesn't end?",
-};
-var q1Answers = {
-    A: "Hyper Text Markup Language", //correct
-    B: "Hit Them Massive Lumbers",
-    C: "High Test Math Liquid",
-    D: "Hyper Technical Mathematical Language",
-};
-var q2Answers = {
-    A: "Candid Screen Standards",
-    B: "Cascading Style Sheets", //correct
-    C: "Creating Slick Styles",
-    D: "Creating Style Simply",
-};
-var q3Answers = {
-    A: "No relation twixt the two", //correct
-    B: "Both are named after coffee",
-    C: "Both were invented on the island of Java",
-    D: "Java requires no scripting",
-};
-var q4Answers = {
-    A: "Properties that influence the styling and layout of HTML elements",
-    B: "Good manners, politeness, pride without showboating, empathy, humility, and an abundance of self - control",
-    C: "An attribute used to define a group of HTML elements in order to apply unique styling and formatting to those elements", //correct
-    D: "Specifies how to style the targeted HTML elements",
-};
-var q5Answers = {
-    A: "Parses Jasons",
-    B: "Converts infidels into believers",
-    C: "Converts numbers into strings",
-    D: "Converts strings into numbers", //correct
+var quizQuestions = [
+    //q1
+    {
+        q: "What does the acronym HTML stand for?",
+        qAnswers: ["A: Hyper Text Markup Language", "B: Hit Them Massive Lumbers", "C: High Test Math Liquid", "D: Hyper Technical Mathematical Language"],
+        qCorrect: "A: Hyper Text Markup Language",
+    },
+    //q2
+    {
+        q: "What does the acronym CSS stand for?",
+        qAnswers: ["A: Candid Screen Standards", "B: Cascading Style Sheets", "C: Creating Slick Styles", "D: Creating Style Simply"],
+        qCorrect: "B: Cascading Style Sheets",
+    },
+    //q3
+    {
+        q: "How does Java relate to JavaScript?",
+        qAnswers = ["A: No relation twixt the two", "B: Both are named after coffee", "C: Both were invented on the island of Java", "D: Java requires no scripting"],
+        qCorrect: "A: No relation twixt the two",
+    },
+    //q4
+    {
+        q: "In CSS, what defines class?",
+        qAnswers = ["A: Properties that influence the styling and layout of HTML elements", "B: Good manners, politeness, pride without showboating, empathy, humility, and an abundance of self - control", "C: An attribute used to define a group of HTML elements in order to apply unique styling and formatting to those elements", "D: Specifies how to style the targeted HTML elements"],
+        qCorrect: "C: An attribute used to define a group of HTML elements in order to apply unique styling and formatting to those elements",
+    },
+    //q5
+    {
+        q: "In Javascript, what does JSON.parse do?",
+        qAnswers = ["A: Parses Jasons", "B: Converts infidels into believers", "C: Converts numbers into strings", "D: Converts strings into numbers"],
+        qCorrect: "D: Converts strings into numbers",
+    },
+    //q6
+    {
+        q: "In HTML, what does the <p> tag represent?",
+        qAnswers = ["A: Parentheses", "B: Paragraph", "C: Processing", "D: Penguin"],
+        qCorrect: "B: Paragraph",
+    },
+    //q7
+    {
+        q: "Which of these If statements is in the correct order?",
+        qAnswers: ["A: Else, If, Else if", "B: Else if, If, Else", "C: If, Else, Else if", "D: If, Else if, Else"],
+        qCorrect: "C: If, Else, Else if",
+    },
+    //q8
+    {
+        q: "Which is NOT a data type?",
+        qAnswers = ["A: Number", "B: Boolean", "C: String", "D: Data"],
+        qCorrect: "D: Data",
+    },
+    //q9
+    {
+        q: "Which of these stops a loop?",
+        qAnswers = ["A: Return", "B: Break", "C: Stop", "D: Cease"],
+        qCorrect: "B: Break",
+    },
+    //q10
+    {
+        q: "What is the terminology for a loop that doesn't end?",
+        qAnswers = ["A: Endless Cycling", "B: Interminable Iteration", "C: Infinite Loop", "D: Unceasing Loop of Horror"],
+        qCorrect: "C: Infinite Loop",
+    },
+];
 
-};
-var q6Answers = {
-    A: "Parentheses",
-    B: "Paragraph", //correct
-    C: "Processing",
-    D: "Penguin",
-};
-var q7Answers = {
-    A: "Else, If, Else if",
-    B: "Else if, If, Else",
-    C: "If, Else, Else if",
-    D: "If, Else if, Else", //correct
+function nextQuestion(index) {
+    var container1 = document.getElementsByClassName("answers");
+    var parent = document.getElementsByClassName("q-a");
+    container1.remove();
+    var container = document.createElement("div");
+    container.classList.add("answers");
+    var current = quizQuestions[index];
+    //display question current.q using createelement like line 126 and assign text content to it
 
-};
-var q8Answers = {
-    A: "Number",
-    B: "Boolean",
-    C: "String",
-    D: "Data", //correct
+    for (var i = 0; i < current.qAnswers.length; i++) {
+        //createElement append to container, add event listener buttonHandler
+        // for loop here vv for answer buttons (append to container)
+        // hard part right here
+    }
 
-};
-var q9Answers = {
-    A: "Return",
-    B: "Break", //correct
-    C: "Stop",
-    D: "Cease",
-};
-var q10Answers = {
-    A: "Endless Cycling",
-    B: "Interminable Iteration",
-    C: "Infinite Loop", //correct
-    D: "Unceasing Loop of Horror",
-};
-
-
-function quiz() {
-    // check if answer was correct then preesnt next quesion and possible answers
-    for (var i = 0; i < quizQuestions.length; i++) {
-        if (quizQuestions[i]) { //finish the iteration
-
-        }
-    };
-    questionEl.textContent = quizQuestions.one; //iterated and indexed questions
-    answerAEl.textContent = q1Answers.A;
+    questionEl.textContent = quizQuestions.q1; //iterated and indexed questions
+    answerAEl.textContent = current. //whatever;
     answerBEl.textContent = q1Answers.B;
     answerCEl.textContent = q1Answers.C;
     answerDEl.textContent = q1Answers.D;
 
+    parent.append(container);
+};
+
+function buttonHandler() {
+    //logic for comparing questions and answers goes in here. deduct in here too w new conditional. need check for time
+    if (QuestIndex + 1 <= quizQuestions.length) {
+        nextQuestion(QuestIndex + 1);
+    } else {
+        endGame();
+    }
 
 };
 
@@ -153,12 +157,10 @@ function endGame() {
         var initials = window.prompt("You've set a new high score! Please enter your initials");
         initials = localStorage.setItem("initials", initials);
     }
-
-
-}
-
+};
 
 startButtonEl.addEventListener("click", quizStart);
+
 
 
 
